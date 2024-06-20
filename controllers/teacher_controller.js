61177f37e71cd69c340627da54c645ff1d8b25f9
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2024-06-20 12:20:34 -0700

    Plus data to rooms

--- controllers/teacher_controller.js
+++ controllers/teacher_controller.js
@@ -14,20 +14,50 @@ module.exports.controller = (app, io, socket_list) => {
         
         if (token) {
             let userId = await getUserId(token);
-            console.log(userId)
 
             if (userId) {
-                console.log(userId[0].user_id);
-                db.query('SELECT * FROM persons WHERE id=?', [userId[0].user_id], (error, result) => {
+                db.query('SELECT * FROM persons WHERE id=?', [userId[0].user_id], (error, result_user) => {
                     if (error) { rejeitado(error.code); return; }
-                    if (result) {
-                        console.log(result);
-                        json = {
-                            auth: 'true',
-                            name: result[0].name,
-                            avatar: result[0].avatar
-                        };
-                        res.status(200).json(json);
+                    if (result_user[0]) {
+                        let dataAtual = new Date();
+                        let ano = dataAtual.getFullYear();
+                        let mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+                        let dia = String(dataAtual.getDate()).padStart(2, '0');
+
+                        let dateF = `${ano}/${mes}/${dia}`;
+
+                        db.query('select loc, init, room_id from connection where user_id = ? and day > ? order by day asc, init asc limit 1;', [userId[0].user_id, dateF], (error, result_room) => {
+                            if (error) { rejeitado(error.code); return; }
+                            if (result_room[0]) {
+                                json = {
+                                    auth: 'true',
+                                    name: result_user[0].name,
+                                    avatar: result_user[0].avatar,
+                                    mode: result_user[0].mode,
+                                    next_class: {
+                                        init: result_room[0].init,
+                                        local: result_room[0].loc,
+                                        class: result_room[0].room_id
+                                    }
+                                };
+                                console.log(json);
+
+                                res.status(200).json(json);
+                            } else {
+                                json = {
+                                    auth: 'true',
+                                    name: result_user[0].name,
+                                    avatar: result_user[0].avatar,
+                                    mode: result_user[0].mode,
+                                    next_class: {
+                                        init: "A definir",
+                                        local: "A definir",
+                                        class: "A definir"
+                                    }
+                                };
+                                res.status(200).json(json);
+                            }
+                        });
                     } else {
                         res.status(401).json({ msg: 'Usuáro não encontrado' });
                     }
