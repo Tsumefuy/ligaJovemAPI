8af5fb1a1f0852abafe7abd49790d127859bd456
Gustavo Maia <101567898+Tsumefuy@users.noreply.github.com>
2024-06-21 15:28:53 -0300

    organize

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const roomsController = require('../controllers/rooms_controller');
 
 router
-    .get('/api/rooms', roomsController.controller);*/
+    .get('/api/rooms', roomsController.controller);
 
 module.exports = router;
