210f7825b6380da692559919856ac740b85f6ef6
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2024-06-22 19:28:15 -0300

    Separeta in function

--- controllers/teacher_controller.js
+++ controllers/teacher_controller.js
@@ -78,70 +78,75 @@ module.exports.controller = (app) => {
     });
 
     app.get('/api/teacher/context', serverServices.verifyJWT, async (req, res) => {
-        let json;
-
         let token= req.headers['authorization'];
     
         let userId = await serverServices.getUserIdByToken(token);
 
         if (userId[0]) {
-            let dataAtual = new Date();
-            let ano = dataAtual.getFullYear();
-            let mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-            let dia = String(dataAtual.getDate()).padStart(2, '0');
-
-            let dateF = `${ano}/${mes}/${dia}`;
-
-            let data = new Date(); // Obtém o momento atual
-            let horas = data.getHours();
-            let minutos = data.getMinutes();
-            let hhmm = `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}`;
-
-            db.query('SELECT name FROM persons WHERE id=?', [userId[0].user_id], (error, result_user) => {
-                if (error) { rejeitado(error.code); return; }
-                if (result_user[0]) {
-                    json = {
-                        name: result_user[0].name,
-                        time: hhmm,
-                        date: dateF,
-                        rooms: []
-                    }
-
-                    db.query('select distinct rooms.id, rooms.course, rooms.mode from rooms inner join connection on rooms.id = connection.room_id and connection.user_id = ?', [userId[0].user_id], (error, result_rooms) => {
-                        if (error) { rejeitado(error.code); return; }
-                        for (let i=0; i < result_rooms.length; i++) {
-                            let data = {
-                                id: result_rooms[i].id,
-                                course: result_rooms[i].course,
-                                type: ((result_rooms[i].mode == "E") ? "Ensino médio" : "Curso Técnico"),
-                                days: []
-                            }
-                            db.query('select day, init, end, loc from connection where room_id = ? and user_id = ?', [result_rooms[i].id, userId[0].user_id], (error, result_rmData) => {
-                                if (error) { rejeitado(error.code); return; }
-                                for (let c=0; c < result_rmData.length; c++) {
-                                    data.days.push({
-                                        day: result_rmData[c].day,
-                                        init: result_rmData[c].init,
-                                        end: result_rmData[c].end,
-                                        local: result_rmData[c].loc
-                                    });
-                                    
-                                }
-                                json.rooms.push(data);
-                                if (i == result_rooms.length-1) {
-                                    console.log(json);
-                                    res.status(200).json(json);
-                                }
-                            });
-                        }
-                        
-                    })
-                }
-            });
+            let response = await getContext(userId[0].userId);
+            console.log(response);
+            res.status(200).json(response);
         } else {
             res.status(400).json({ msg: 'Token invalido!' });
         }
         
     });
 
+}
+
+async function getContext(userId) {
+    console.log(userId);
+    let dataAtual = new Date();
+    let ano = dataAtual.getFullYear();
+    let mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+    let dia = String(dataAtual.getDate()).padStart(2, '0');
+
+    let dateF = `${ano}/${mes}/${dia}`;
+
+    let data = new Date(); // Obtém o momento atual
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+    let hhmm = `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}`;
+
+    db.query('SELECT name FROM persons WHERE id=?', [userId], (error, result_user) => {
+        if (error) { rejeitado(error.code); return; }
+        if (result_user[0]) {
+            json = {
+                name: result_user[0].name,
+                time: hhmm,
+                date: dateF,
+                rooms: []
+            }
+
+            db.query('select distinct rooms.id, rooms.course, rooms.mode from rooms inner join connection on rooms.id = connection.room_id and connection.user_id = ?', [userId], (error, result_rooms) => {
+                if (error) { rejeitado(error.code); return; }
+                for (let i=0; i < result_rooms.length; i++) {
+                    let data = {
+                        id: result_rooms[i].id,
+                        course: result_rooms[i].course,
+                        type: ((result_rooms[i].mode == "E") ? "Ensino médio" : "Curso Técnico"),
+                        days: []
+                    }
+                    db.query('select day, init, end, loc from connection where room_id = ? and user_id = ?', [result_rooms[i].id, userId], (error, result_rmData) => {
+                        if (error) { rejeitado(error.code); return; }
+                        for (let c=0; c < result_rmData.length; c++) {
+                            data.days.push({
+                                day: result_rmData[c].day,
+                                init: result_rmData[c].init,
+                                end: result_rmData[c].end,
+                                local: result_rmData[c].loc
+                            });
+                            
+                        }
+                        json.rooms.push(data);
+                        if (i == result_rooms.length-1) {
+                            console.log(json);
+                            return(json);
+                        }
+                    });
+                }
+                
+            })
+        }
+    });
 }
