69b4329fa23b7fb7bd9491d5452fc13099576c7e
Gustavo Maia <101567898+Tsumefuy@users.noreply.github.com>
2024-06-27 00:38:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require('compression');
 const cors = require('cors');
 var  fs = require('fs');
 
@@ -15,6 +16,7 @@ var server = require('http').createServer(app);
 
 var serverPort = process.env.PORT || 8080;
 
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
