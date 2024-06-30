4aa212d03afb212db0d2b15245e2b2c371826bb9
Gustavo Maia <101567898+Tsumefuy@users.noreply.github.com>
2024-06-29 21:48:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-Servidor HTTPS
+//Servidor HTTPS
 https.createServer(sslOptions, app).listen(httpsPort, () => {
   console.log(`Servidor HTTPS rodando na porta ${httpsPort}!`);
 });
